710f0f3d54f936f7b35d29d4ac28939bbf37b878
Hakan Bilgic <hbilgic1992@gmail.com>
2022-08-05 11:07:47 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import react from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Components/App";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
