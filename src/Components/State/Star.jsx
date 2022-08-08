87a74db45d309657fddcc6bba37428180afc1a58
Hakan Bilgic <hbilgic1992@gmail.com>
2022-08-08 08:45:08 +0200

    color stars on hover

--- src/Components/State/Star.jsx
+++ src/Components/State/Star.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
 import { FaStar } from "react-icons/fa";
 const Star = ({ selected = false, onSelect = (f) => f }) => {
-  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
+  return <StarIcon color={selected ? "red" : "grey"} onClick={onSelect} />;
 };
+
+const StarIcon = styled(FaStar)`
+  font-size: 25px;
+  :hover {
+    fill: red;
+    cursor: pointer;
+  }
+`;
+
 export default Star;
